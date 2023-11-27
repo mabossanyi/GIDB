9f214b609faa0e31d015ed92fde9874cb7da016d
Marc-Andre Bossanyi <ma.bossanyi@gmail.com>
2023-11-26 19:47:32 -0500

    Added the file related to the DROP scripts for the database

---
+++ SQL/DROP.sql
@@ -0,0 +1,17 @@
+/*
+	Author: Marc-Andre Bossanyi
+	Email: ma.bossanyi@gmail.com
+	Creation Date: 2023/11/26
+	Last Updated: 2023/11/26
+*/
+
+-- Script DROP for the tables
+DROP TABLE ItemsSet;
+DROP TABLE CharacterItem;
+DROP TABLE CharacterStat;
+DROP TABLE Character;
+DROP TABLE Element;
+DROP TABLE Weapon;
+DROP TABLE Item;
+DROP TABLE Stat;
+DROP TABLE Slot;
