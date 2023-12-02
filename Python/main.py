56209c798cd6268980ab0be8f742593519af9c56
Marc-Andre Bossanyi <ma.bossanyi@gmail.com>
2023-12-02 04:18:56 -0500

    Updated the files related to the Python script by adding verbosity using 'print' methods and by verifying the 'INSERT.sql' has been written

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,8 @@ from writer import Writer
 def main():
     # Header of the program
     version = "TODO"
+    print("Running the Genshin Impact Database (GIDB) script using "
+          "the version '{}'\n".format(version))
 
     # Get the class "Extractor" for the main page using the URL
     # to extract the HTML
@@ -20,9 +22,11 @@ def main():
     storage = Storage()
 
     # Extract and store the elements
+    print("Extracting and storing the elements...")
     extract_and_store_elements(main_page_extractor, storage)
 
     # Extract and store the weapons
+    print("Extracting and storing the weapons...")
     extract_and_store_weapons(main_page_extractor, storage)
 
     # Get the class "Extractor" for the artifacts page using the URL
@@ -31,12 +35,15 @@ def main():
     artifacts_page_extractor = get_page_extractor_from_url(artifacts_page_url)
 
     # Extract and store the items (i.e. the artifact names)
+    print("Extracting and storing the items (artifacts)...")
     extract_and_store_items(artifacts_page_extractor, storage)
 
     # Extract and store the items sets (i.e. the artifact sets description)
+    print("Extracting and storing the items sets...")
     extract_and_store_items_sets(artifacts_page_extractor, storage)
 
     # Extract and store the characters data for each character on the main page
+    print("Extracting and storing the characters data for each character...")
     extract_and_store_characters_data(
         main_page_url, main_page_extractor, storage)
 
@@ -44,22 +51,28 @@ def main():
     data_processor = processor.Processor(storage.get_characters_raw_data())
 
     # Preprocess and store the stats from the characters raw data
+    print("Preprocessing and storing the stats...")
     preprocess_and_store_stats(data_processor, storage)
 
     # Preprocess and store the slots from the characters raw data
+    print("Preprocessing and storing the slots...")
     preprocess_and_store_slots(data_processor, storage)
 
     # Preprocess and store the characters from the characters raw data
+    print("Preprocessing and storing the characters...")
     preprocess_and_store_characters(data_processor, storage)
 
     # Preprocess and store the characters stats from the characters raw data
+    print("Preprocessing and storing the characters-stats...")
     preprocess_and_store_characters_stats(data_processor, storage)
 
     # Preprocess and store the characters items from the characters raw data
+    print("Preprocessing and storing the characters-items...")
     preprocess_and_store_characters_items(data_processor, storage)
 
     # Write the "INSERT.sql" file
     file_name = "INSERT.sql"
+    print("Writing the 'INSERT.sql' file...")
     write_insert_sql_file(file_name, version, storage)
 
 
@@ -74,58 +87,86 @@ def get_page_extractor_from_url(url):
 def extract_and_store_elements(page_extractor, storage):
     elements = page_extractor.extract_elements()
     storage.store_elements(elements)
+    _print_verbosity_from_data(elements)
+    print(" Completed.\n\t --> Total of {} element(s)\n".format(len(elements)))
 
 
 def extract_and_store_weapons(page_extractor, storage):
     weapons = page_extractor.extract_weapons()
     storage.store_weapons(weapons)
+    _print_verbosity_from_data(weapons)
+    print(" Completed.\n\t --> Total of {} weapon(s)\n".format(len(weapons)))
 
 
 def extract_and_store_items(artifacts_extractor, storage):
-    items_names = artifacts_extractor.extract_artifacts_name()
-    storage.store_items(items_names)
+    item_names = artifacts_extractor.extract_artifacts_name()
+    storage.store_items(item_names)
+    _print_verbosity_from_data(item_names, title="Artifacts")
+    print(" Completed.\n\t --> Total of {} item(s)\n".format(len(item_names)))
 
 
 def extract_and_store_items_sets(artifacts_extractor, storage):
     items_sets = artifacts_extractor.extract_artifacts_description()
     storage.store_items_sets(items_sets)
+    print(" Completed.\n\t --> Total of {} items set(s)\n".format(
+        len(items_sets)))
 
 
 def extract_and_store_characters_data(url, page_extractor, storage):
     characters_raw_data = page_extractor.extract_characters_raw_data(url)
     storage.store_characters_raw_data(characters_raw_data)
+    print(" Completed.\n\t --> Total of {} character(s) data\n".format(
+        len(characters_raw_data)))
 
 
 def preprocess_and_store_stats(data_processor, storage):
     stats = data_processor.preprocess_characters_raw_data_for_stat()
     storage.store_stats(stats)
+    _print_verbosity_from_data(stats)
+    print(" Completed.\n\t --> Total of {} stat(s)\n".format(len(stats)))
 
 
 def preprocess_and_store_slots(data_processor, storage):
     slots = data_processor.preprocess_characters_raw_data_for_slot()
     storage.store_slots(slots)
+    _print_verbosity_from_data(slots)
+    print(" Completed.\n\t --> Total of {} slot(s)\n".format(len(slots)))
 
 
 def preprocess_and_store_characters(data_processor, storage):
     characters = data_processor.preprocess_characters_raw_data_for_character()
     storage.store_characters(characters)
+    print(" Completed.\n\t --> Total of {} character(s)\n".format(
+        len(characters)))
 
 
 def preprocess_and_store_characters_stats(data_processor, storage):
     characters_stats = (
         data_processor.preprocess_characters_raw_data_for_characters_stat())
     storage.store_characters_stats(characters_stats)
+    print(" Completed.\n\t --> Total of {} characters-stat(s)\n".format(
+        len(characters_stats)))
 
 
 def preprocess_and_store_characters_items(data_processor, storage):
     characters_items = (
         data_processor.preprocess_characters_raw_data_for_characters_items())
     storage.store_characters_items(characters_items)
+    print(" Completed.\n\t --> Total of {} characters-item(s)\n".format(
+        len(characters_items)))
 
 
 def write_insert_sql_file(file_name, version, storage):
     file_writer = Writer()
     file_writer.write_insert_sql_file(file_name, version, storage)
+    print(" Completed.")
+
+
+def _print_verbosity_from_data(data_list, title=""):
+    if title != "":
+        print("  {}:".format(title))
+
+    [print("\t> {}".format(data)) for data in data_list]
 
 
 # Press the green button in the gutter to run the script.
